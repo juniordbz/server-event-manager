cc85d12a016d1504217f13e6d7bf59547a67ef08
juniordbz <franciscojunior02@hotmail.com.br>
2024-04-06 16:08:03 -0300

    add route - registerForEvent

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createEvent } from "./routes/create-event";
+import { resisterForEvent } from "./routes/register-for-events";
 
 const app = fastify()
 
@@ -9,6 +10,7 @@ const app = fastify()
 
 
   app.register(createEvent)
+  app.register(resisterForEvent)
 
 app.listen({port: 3333}).then(() => {
   console.log("Server is running on port 3333")
